2d239bd6840c27caf91de2e5ebb02316f56c5f32
Daniel Nygaard <dani12h1@stud.kea.dk>
2023-11-18 12:53:16 +0100

    Boks virker

--- src/app/components/progressBar.jsx
+++ src/app/components/progressBar.jsx
@@ -1,59 +1,114 @@
 "use client";
-
-import React from "react";
+import supabase from "../config/supabaseClient";
+import { useEffect, useState } from "react";
 import * as Progress from "@radix-ui/react-progress";
-import { useEffect } from "react";
+import * as Slider from "@radix-ui/react-slider";
 
-export const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0aXlqZmhwbG53bHJ5d3F5YmptIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5OTY5MzczNywiZXhwIjoyMDE1MjY5NzM3fQ.eYFo0PzX08UuW9HtGpF2Zc2vEEz-CHq7TfXCqVSuBz4";
-export const ENDPOINT = "https://atiyjfhplnwlrywqybjm.supabase.co/rest/v1/progress";
+const ProgressBar = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [data, setData] = useState(null);
+  const [selectedSliderValue, setSelectedSliderValue] = useState(0);
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
 
-const ProgressDemo = () => {
-  const [progress, setProgress] = React.useState(13);
-  console.log("hheeeey toppen");
+  const handleSliderChange = (value) => {
+    setSelectedSliderValue(value);
+  };
+
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await fetch(ENDPOINT, {
-          method: "GET",
-          headers: {
-            apikey: API_KEY,
-          },
-        });
-        const data = await response.json();
-        console.log(data);
-        console.log("hheeeey");
+      const { data, error } = await supabase.from("progress").select();
 
-        // Use data to update progress (this is just an example, replace it with your logic)
-        setProgress(data.progress);
-      } catch (error) {
-        console.error("Error fetching data:", error);
+      if (error) {
+        setFetchError("Failed to fetch data");
+        setData(null);
+        console.log(error);
+      }
+
+      if (data) {
+        setData(data);
+        setFetchError(null);
       }
     };
 
-    function callButton() {}
-
-    // Call the fetchData function
     fetchData();
-  }, []); // Empty dependency array means this effect runs once when the component mounts
-
-  React.useEffect(() => {
-    const timer = setTimeout(() => setProgress(66), 500);
-    return () => clearTimeout(timer);
   }, []);
+  console.log(data);
+
+  const totalAmount = data ? data.reduce((acc, doner) => acc + doner.amount, 0) : 0;
+  const progressPercentage = (totalAmount / 10000) * 100;
+
+  const handleSupportClick = async () => {
+    // Extract the first element from the array if it's an array
+    const sliderValue = Array.isArray(selectedSliderValue) ? selectedSliderValue[0] : selectedSliderValue;
+
+    // Pusher input data til supabase
+    try {
+      const { data, error } = await supabase.from("progress").upsert([
+        {
+          name,
+          phonenumber: phoneNumber,
+          amount: sliderValue,
+          email: email, // Tilføjer den valgte mængde fra silderValue
+        },
+      ]);
+
+      if (error) {
+        console.error("Error pushing data to the database:", error);
+      } else {
+        console.log("Data successfully pushed to the database:", data);
+      }
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  };
 
   return (
-    <>
-      <Progress.Root
-        className="relative overflow-hidden bg-blackA6 rounded-full w-[300px] h-[25px]"
-        style={{
-          transform: "translateZ(0)",
-        }}
-        value={progress}
-      >
-        <Progress.Indicator className="bg-white w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]" style={{ transform: `translateX(-${100 - progress}%)` }} />
-      </Progress.Root>
-    </>
+    <div className="bg-white flex justify-center items-center w-screen h-screen">
+      <div className="bg-green-800 h-[40vh] p-4">
+        {fetchError && <p>{fetchError}</p>}
+        {data && (
+          <div>
+            <p className="pb-3" key={data.amount}>
+              {" "}
+              {totalAmount} kvm købt ud af 10000
+            </p>
+            <Progress.Root
+              className="relative overflow-hidden bg-white rounded-full w-[300px] h-[25px]"
+              style={{
+                transform: "translateZ(0)",
+              }}
+              value={progressPercentage}
+            >
+              <Progress.Indicator className="bg-green-500 w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]" style={{ transform: `translateX(-${100 - progressPercentage}%)` }} />
+            </Progress.Root>
+          </div>
+        )}
+        <div className="pt-3">
+          <h3>Vælg antal kvm du vil redde</h3>
+        </div>
+        <div className="pt-3">
+          <Slider.Root className="relative flex items-center select-none touch-none w-[200px] h-5" defaultValue={[50]} onValueChange={handleSliderChange} max={10000} step={1}>
+            <Slider.Track className="bg-white relative grow rounded-full h-[3px]">
+              <Slider.Range className="absolute bg-white rounded-full h-full" />
+            </Slider.Track>
+            <Slider.Thumb className="block w-5 h-5 bg-white shadow-blackA4 rounded-[10px] hover:bg-violet3 focus:outline-none focus:shadow-blackA5" aria-label="Volume" />
+          </Slider.Root>
+          <p className="py-2 ">Antal kvm valgt {selectedSliderValue} </p>
+        </div>
+        <div className="flex flex-col gap-3">
+          <input className="p-1 pt-3 text-black" placeholder="name" type="text" onChange={(e) => setName(e.target.value)} />
+          <input className="p-1 pt-3 text-black" placeholder="phonenumber" type="text" onChange={(e) => setPhoneNumber(e.target.value)} />
+          <input className="p-1 pt-3 text-black" placeholder="email" type="text" onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="pt-3"></div>
+        <button className="p-2 bg-green-500 text-white" onClick={handleSupportClick}>
+          STØT
+        </button>
+      </div>
+    </div>
   );
 };
 
-export default ProgressDemo;
+export default ProgressBar;
---
+++ src/app/home/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProgressBar from "../components/progressBar";
+
+function page() {
+  return (
+    <div>
+      <ProgressBar />
+    </div>
+  );
+}
+
+export default page;
