dd8f3d1e0625308620cecced3531246d58326858
Daniel Nygaard <dani12h1@stud.kea.dk>
2023-11-18 10:37:33 +0100

    Før fuck op

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+// import { createClient } from "@/utils/supabase/server";
+// import { cookies } from "next/headers";
+
+// export default async function Notes() {
+//   const cookieStore = cookies();
+//   const supabase = createClient(cookieStore);
+//   const { data: notes } = await supabase.from("notes").select();
+
+//   return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+// }
