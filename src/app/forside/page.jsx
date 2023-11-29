9268221f1393b6a41f0853b2432c01f24ba55019
Luuna Nørgaard <luun0004@stud.kea.dk>
2023-11-29 16:03:15 +0100

    forside

--- src/app/forside/page.jsx
+++ src/app/forside/page.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import ProgressBar from "../components/progressBar";
 
 function page() {
   return (
     <>
-      <h1>hej</h1>
-      <div>
-        <ProgressBar />
+      <div className='w-screen h-screen flex justify-center items-center'>
+        <h1>hej</h1>
       </div>
     </>
   );
