caf1934b6de60e2033a5d17e6fb753983ba5e865
Luuna Nørgaard <luun0004@stud.kea.dk>
2023-11-30 11:25:26 +0100

    billeder

---
+++ src/app/components/card.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image';
+function card() {
+  return (
+    <>
+      <section>
+        <h1>Sådan kan du støtte vores arbejde</h1>
+        <div>
+            <div>
+               <Image 
+               src={=/}>
+                
+               </Image>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default card
