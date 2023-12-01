64b49c29492ecae5c3a652282e45999ca25e50ad
Luuna Nørgaard <luun0004@stud.kea.dk>
2023-12-01 13:35:02 +0100

    footerPT

--- src/app/pages/forside/page.jsx
+++ src/app/pages/forside/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Card from "../../components/Card";
+import Footer from "../../components/Footer";
 
 function page() {
   return (
     <>
       <Card />
+      <Footer />
     </>
   );
 }
