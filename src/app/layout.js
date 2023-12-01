7b7965918839013254dd920c083d30ad99920865
Daniel Nygaard <dani12h1@stud.kea.dk>
2023-12-01 15:31:01 +0100

    footer test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Navigation />
-      <body>{children}</body>
+      <Footer />
+
+      <body className="bg-hvid">{children}</body>
     </html>
   );
 }
