017118b1bb247e3816e2137ff2a1afde6967447c
Luuna Nørgaard <luun0004@stud.kea.dk>
2023-12-04 11:55:31 +0100

    skygger

--- src/app/pages/forside/page.jsx
+++ src/app/pages/forside/page.jsx
@@ -19,8 +19,8 @@ const Card = () => {
             <button className=' text-orange pt-2'>Læs mere →</button>
           </section>
           <section className='w-full h-full border-t-2 border-black'>
-            <h1>Sådan kan du støtte vores arbejde</h1>
             <div className='m-3 flex-col justify-center'>
+              <h1>Sådan kan du støtte vores arbejde</h1>
               <div>
                 <Image src={truck} width={500} height={500} alt='Picture of workers' />
                 <h3>Doner til vores arbejde</h3>
@@ -53,51 +53,49 @@ const Card = () => {
               </div>
             </div>
           </section>
-          <section>
-            <h1>Aktuelt nyt</h1>
+          <section className=''>
             <div className='m-3 flex-col justify-center'>
-              <div>
+              <h1>Aktuelt nyt</h1>
+              <div className='shadow-xl p-3'>
                 <Image src={pink} width={500} height={500} alt='Picture of origin people' />
-                <p>6 NOVEMBER 2023</p>
-                <div>
+                <div className='flex'>
+                  <p>6 NOVEMBER 2023</p>
                   <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt-fill' viewBox='0 0 16 16'>
                     <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
                   </svg>
-
                   <p>Pico Bonito </p>
                 </div>
                 <h3>Støtte af tolupanfolket</h3>
                 <p> Oprindelige folk og civilsamfundet i Honduras blev ikke kun udfordret af sundhedsrelateret pres under COVID-19 pandemien.</p>
                 <button className='text-orange pt-2'>Læs mere →</button>
               </div>
-              <div>
+              <div className='shadow-xl p-3'>
                 <Image src={people} width={500} height={500} alt='Picture of people in the forest' />
-                <div>
+                <div className='flex'>
                   <p>20 NOVEMBER 2023</p>
-                  <div>
-                    <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt-fill' viewBox='0 0 16 16'>
-                      <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
-                    </svg>
-                    <p>Etiopien</p>
-                  </div>
+                  <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt-fill' viewBox='0 0 16 16'>
+                    <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
+                  </svg>
+                  <p>Etiopien</p>
                 </div>
                 <h3>Nyt initiativ: forbedring af skovovervågning</h3>
                 <p>Verdens Skove arbejder på et projekt i det sydvestlige Etiopien.</p>
                 <button className='text-orange pt-2'>Læs mere →</button>
               </div>
-              <Image src={foresttruck} width={500} height={500} alt='Picture of  forest and truck' />
 
-              <div className='flex'>
-                <p>18 SEPTEMBER 2023</p>
-                <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt-fill' viewBox='0 0 16 16'>
-                  <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
-                </svg>
-                <p>Bolivia</p>
+              <div className='shadow-xl p-3'>
+                <Image src={foresttruck} width={500} height={500} alt='Picture of forest and truck' />
+                <div className='flex'>
+                  <p>18 SEPTEMBER 2023</p>
+                  <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt-fill' viewBox='0 0 16 16'>
+                    <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
+                  </svg>
+                  <p>Bolivia</p>
+                </div>
+                <h3>Tropiske skove forsvinder</h3>
+                <p>Nye tal viser en bekymrende udvikling i tab af skovdække i Bolivia. </p>
+                <button className='text-orange pt-2'>Læs mere →</button>
               </div>
-              <h3>Tropiske skove forsvinder</h3>
-              <p>Nye tal viser en bekymrende udvikling i tab af skovdække i Bolivia. </p>
-              <button className='text-orange pt-2'>Læs mere →</button>
-              <div></div>
             </div>
           </section>
         </div>
