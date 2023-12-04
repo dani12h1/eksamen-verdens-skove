ed0701ef3ff615eda8a0e8c33184fb4f099fe857
Luuna Nørgaard <luun0004@stud.kea.dk>
2023-12-04 14:01:21 +0100

    hej

--- src/app/pages/forside/page.jsx
+++ src/app/pages/forside/page.jsx
@@ -11,103 +11,113 @@ import foresttruck from "../../billeder/treesandtruck.png";
 const Card = () => {
   return (
     <>
-      <main className="leading-7">
-        <div className="bg-white p-4">
-          <section className="p-3 text-black">
-            <h1 className="">Hvem er Verdens Skove? </h1>
+      <main className='leading-7'>
+        <div className='bg-white p-4'>
+          <section className='p-3 text-black'>
+            <h1 className=''>Hvem er Verdens Skove? </h1>
             <p>Verdens Skove er en dansk miljøorganisation, der gennem 40 år har arbejdet for en verden med en rig skovnatur gennem bl.a. bevarelse af skovene. Vores fokus er på klima, biodiversitet og de oprindelige folks rettigheder. </p>
-            <button className=" text-orange pt-2">Læs mere →</button>
+            <button className=' text-orange pt-2'>Læs mere →</button>
           </section>
-          <section className="w-full h-full border-t-2 border-black">
-            <div className="m-3 flex-col justify-center">
+          <section className='w-full h-full border-t-2 border-black'>
+            <div className='m-3 flex-col justify-center gap-4 '>
               <h1>Sådan kan du støtte vores arbejde</h1>
-              <div className="shadow-xl ">
-                <Image src={truck} width={500} height={500} alt="Picture of workers" />
-                <h3>Doner til vores arbejde</h3>
-                <p className="p-2">Giv et valgfrit beløb, og vær med til at redde skoven nu!</p>
-                <button className="text-orange p-2">Doner her →</button>
+              <div className='gap-4'>
+                <div className='shadow-xl gap-4'>
+                  <Image src={truck} width={500} height={500} alt='Picture of workers' />
+                  <div className='p-4'>
+                    <h3>Doner til vores arbejde</h3>
+                    <p>Giv et valgfrit beløb, og vær med til at redde skoven nu!</p>
+                    <button className='text-orange'>Doner her →</button>
+                  </div>
+                </div>
               </div>
-              <div className="shadow-xl ">
-                <Image src={totebag} width={500} height={500} alt="Picture of totebag" />
-                <h3>Besøg vores webshop</h3>
-                <p>Støt os ved køb af plakater, kaffe og meget mere..</p>
-                <button className="text-orange">Gå til webshop →</button>
+              <div className='shadow-xl'>
+                <Image src={totebag} width={500} height={500} alt='Picture of totebag' />
+
+                <div className='p-4'>
+                  <h3>Besøg vores webshop</h3>
+                  <p>Støt os ved køb af plakater, kaffe og meget mere..</p>
+                  <button className='text-orange'>Gå til webshop →</button>
+                </div>
               </div>
-              <div className="shadow-xl ">
-                <Image src={certificat} width={500} height={500} alt="Picture of certificate" />
-                <h3>Regnskovscertifikat</h3>
-                <p>Køb et stykke jord og hjælp med at bevare regnskoven</p>
-                <button className="text-orange">Køb her →</button>
+              <div className='shadow-xl '>
+                <Image src={certificat} width={500} height={500} alt='Picture of certificate' />
+                <div className='p-4'>
+                  <h3>Regnskovscertifikat</h3>
+                  <p>Køb et stykke jord og hjælp med at bevare regnskoven</p>
+                  <button className='text-orange'>Køb her →</button>
+                </div>
               </div>
-              <button className="text-orange">Se flere støttemuligheder →</button>
+
+              <button className='text-orange'>Se flere støttemuligheder →</button>
             </div>
           </section>
-          <section className="mb-12"></section>
+          <section className='mb-12'></section>
           <section>
             <div>
-              <div className="bg-gray-200 p-5">
-                <Image src={bw} width={500} height={500} alt="Picture of the old days" />
+              <div className='bg-gray-200 p-5'>
+                <Image src={bw} width={500} height={500} alt='Picture of the old days' />
                 <h1>Hvordan arbejder vi? </h1>
                 <p>Vi samarbejde med folk, som lever i og af skoven. Vi arbejder i områder med særlig høj biodiversitet. I dag har Verdens Skove indsatser i Danmark, Latinamerika og Østafrika. Verdens Skove samarbejder med lokale partnerorganisationer i det globale syd og har dedikeret gruppe frivillige i Danmark. </p>
-                <button className=" text-orange pt-2">Læs mere →</button>
+                <button className=' text-orange pt-2'>Læs mere →</button>
               </div>
             </div>
           </section>
           <section>
-            <h2 className="flex justify-center">Aktuelt nyt</h2>
-            <div className="flex overflow-scroll ">
-              <div className=" flex justify-center gap-4">
-                <div className=" w-80 shadow-xl">
-                  <Image src={pink} alt="Picture of origin people" />
-                  <div className="flex p-4 justify-between">
+            <h2 className='flex justify-center'>Aktuelt nyt</h2>
+            <div className='flex overflow-scroll '>
+              <div className=' flex justify-center gap-4'>
+                <div className=' w-80 shadow-xl'>
+                  <Image src={pink} alt='Picture of origin people' />
+                  <div className='flex p-4 justify-between'>
                     <p>6 NOVEMBER 2023</p>
-                    <div className="flex">
-                      <svg className="" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                        <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                    <div className='flex'>
+                      <svg className='' xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt-fill' viewBox='0 0 16 16'>
+                        <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
                       </svg>
                       <p>Pico Bonito </p>
                     </div>
                   </div>
-                  <div className="p-4">
+                  <div className='p-4'>
                     <h3>Støtte af tolupanfolket</h3>
                     <p> Oprindelige folk og civilsamfundet i Honduras blev ikke kun udfordret af sundhedsrelateret pres under COVID-19 pandemien.</p>
-                    <button className="text-orange pt-2">Læs mere →</button>
+                    <button className='text-orange pt-2'>Læs mere →</button>
                   </div>
                 </div>
-                <div className=" w-80 shadow-xl">
-                  <Image src={people} alt="Picture of people in the forest" />
-                  <div className="flex p-4 justify-between">
+                <div className=' w-80 shadow-xl'>
+                  <Image src={people} alt='Picture of people in the forest' />
+                  <div className='flex p-4 justify-between'>
                     <p>20 NOVEMBER 2023</p>
-                    <div className="flex">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                        <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                    <div className='flex'>
+                      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt-fill' viewBox='0 0 16 16'>
+                        <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
                       </svg>
                       <p>Etiopien</p>
                     </div>
                   </div>
-                  <div className="p-4">
+                  <div className='p-4'>
                     <h3>Nyt initiativ: forbedring af skovovervågning</h3>
                     <p>Verdens Skove arbejder på et projekt i det sydvestlige Etiopien.</p>
 
-                    <button className="text-orange pt-2">Læs mere →</button>
+                    <button className='text-orange pt-2'>Læs mere →</button>
                   </div>
                 </div>
 
-                <div className="w-80 shadow-xl">
-                  <Image src={foresttruck} alt="Picture of forest and truck" />
-                  <div className="flex p-4 justify-between">
+                <div className='w-80 shadow-xl'>
+                  <Image src={foresttruck} alt='Picture of forest and truck' />
+                  <div className='flex p-4 justify-between'>
                     <p>18 SEPTEMBER 2023</p>
-                    <div className="flex">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                        <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                    <div className='flex'>
+                      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt-fill' viewBox='0 0 16 16'>
+                        <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
                       </svg>
                       <p>Bolivia</p>
                     </div>
                   </div>
-                  <div className="p-4">
+                  <div className='p-4'>
                     <h3>Tropiske skove forsvinder</h3>
                     <p>Nye tal viser en bekymrende udvikling i tab af skovdække i Bolivia. </p>
-                    <button className="text-orange pt-2">Læs mere →</button>
+                    <button className='text-orange pt-2'>Læs mere →</button>
                   </div>
                 </div>
               </div>
