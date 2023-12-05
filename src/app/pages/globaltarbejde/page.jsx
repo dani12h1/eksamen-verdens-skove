a9bed521e5f9db20a56004db0c30cb224a4382e2
Daniel Nygaard <dani12h1@stud.kea.dk>
2023-12-05 15:08:12 +0100

    Halløj på global på arbejde

--- src/app/pages/globaltarbejde/page.jsx
+++ src/app/pages/globaltarbejde/page.jsx
@@ -19,60 +19,59 @@ function globaltarbejde() {
           <Image src={cover} className="w-full h-full" alt="Picture of global work" />
           <h1 className="text-white absolute top-[7rem] left-2 ">Globlat Arbejde</h1>
         </section>
+
+        <section className=" pb-6 p-6">
+          <h2>Verdens Skove globalt</h2>
+          <p>Gennem 30 år har Verdens Skove sammen med 200.000 danskere bevaret 88.300.000 m2, eller hvad der svarer til 12.362 fodboldbaner af regnskov i Costa Rica, Ecuador og Nicaragua. Dermed har Verdens Skove været med til at afværge den katastrofe, der truede regnskovene i slutningen af 80’erne.</p>
+        </section>
+        <section className="bg-lys_grå p-6">
+          <h2 className="p-3">Her arbejder vi</h2>
+          <div className="flex overflow-scroll p-6 gap-5">
+            <div className="rounded-full">
+              <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px]  " src={honduras} alt="Picture of Honduras" />
+              <p className="flex justify-center">Honduras</p>
+            </div>
+            <div>
+              <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={bolivia} alt="Picture of Bolivia" />
+              <p className="flex justify-center"> Bolivia</p>
+            </div>
+            <div>
+              <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={etiopien} alt="Picture of Etiopien" />
+              <p className="flex justify-center">Etiopien</p>
+            </div>
+            <div>
+              <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={Nicaragua} alt="Picture of Nicaragua" />
+              <p className="flex justify-center">Nicaragua</p>
+            </div>
+            <div>
+              <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={Uganda} alt="Picture of Uganda" />
+              <p className="flex justify-center">Uganda</p>
+            </div>
+            <div>
+              <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={panama} alt="Picture of Panama" />
+              <p className="flex justify-center">Panama</p>
+            </div>
+          </div>
+        </section>
         <section className="p-6">
-          <section className=" pb-6">
-            <h2>Verdens Skove globalt</h2>
-            <p>Gennem 30 år har Verdens Skove sammen med 200.000 danskere bevaret 88.300.000 m2, eller hvad der svarer til 12.362 fodboldbaner af regnskov i Costa Rica, Ecuador og Nicaragua. Dermed har Verdens Skove været med til at afværge den katastrofe, der truede regnskovene i slutningen af 80’erne.</p>
-          </section>
-          <section className="bg-lys_grå">
-            <h2 className="p-3">Her arbejder vi</h2>
-            <div className="flex overflow-scroll p-6 gap-5">
-              <div className="rounded-full">
-                <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px]  " src={honduras} alt="Picture of Honduras" />
-                <p className="flex justify-center">Honduras</p>
-              </div>
-              <div>
-                <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={bolivia} alt="Picture of Bolivia" />
-                <p className="flex justify-center"> Bolivia</p>
-              </div>
-              <div>
-                <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={etiopien} alt="Picture of Etiopien" />
-                <p className="flex justify-center">Etiopien</p>
-              </div>
-              <div>
-                <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={Nicaragua} alt="Picture of Nicaragua" />
-                <p className="flex justify-center">Nicaragua</p>
-              </div>
-              <div>
-                <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={Uganda} alt="Picture of Uganda" />
-                <p className="flex justify-center">Uganda</p>
-              </div>
-              <div>
-                <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={panama} alt="Picture of Panama" />
-                <p className="flex justify-center">Panama</p>
-              </div>
-            </div>
-          </section>
-          <section>
-            <div>
-              <Image src={skov} alt="Picture of reainforest" />
-            </div>
-            <div className="lg:">
-              <h2 className="pt-5">Hvorfor er regnskovene vigtige?</h2>
-              <p>Regnskove er afgørende for planetens sundhed og menneskers overlevelse. De huser en unik biodiversitet, med over halvdelen af verdens arter, og er en kilde til medicin og fødevarer. Regnskovene regulerer lokalt og globalt klima ved at producere regn, binde CO2 og forhindre drivhusgasudledning. Desuden er de nøglen til ferskvand, og millioner af mennesker afhænger af dem for deres levebrød. Skovrydning truer ikke kun arter, men destabiliserer også økosystemer og påvirker klimaet. </p>
-              <button className="text-orange pt-5">Læs mere →</button>
-            </div>
-          </section>
-          <section>
-            <div>
-              <Image src={mandiflod} alt="Picture of man in boat" />
-            </div>
-            <div>
-              <h2 className="pt-5">Hvordan vi støtter de oprindelige folk</h2>
-              <p>Oprindelige folk er afgørende samarbejdspartnere for Verdens Skoves bevaringsarbejde i regnskovene. De bebor mange af verdens tilbageværende regnskove, og forskning viser, at skove bevares bedre, hvor oprindelige folk har jordrettigheder. Deres livsstil, mindre afhængig af industriel produktion og vækst, spiller en nøglerolle. Verdens Skove arbejder for at sikre juridiske rettigheder til skovene for oprindelige folk og presse for respekt fra nybyggere, virksomheder og myndigheder. </p>
-              <button className="text-orange pt-5">Læs mere →</button>
-            </div>
-          </section>
+          <div>
+            <Image src={skov} alt="Picture of reainforest" />
+          </div>
+          <div className="lg:">
+            <h2 className="pt-5">Hvorfor er regnskovene vigtige?</h2>
+            <p>Regnskove er afgørende for planetens sundhed og menneskers overlevelse. De huser en unik biodiversitet, med over halvdelen af verdens arter, og er en kilde til medicin og fødevarer. Regnskovene regulerer lokalt og globalt klima ved at producere regn, binde CO2 og forhindre drivhusgasudledning. Desuden er de nøglen til ferskvand, og millioner af mennesker afhænger af dem for deres levebrød. Skovrydning truer ikke kun arter, men destabiliserer også økosystemer og påvirker klimaet. </p>
+            <button className="text-orange pt-5">Læs mere →</button>
+          </div>
+        </section>
+        <section className="p-6">
+          <div>
+            <Image src={mandiflod} alt="Picture of man in boat" />
+          </div>
+          <div>
+            <h2 className="pt-5">Hvordan vi støtter de oprindelige folk</h2>
+            <p>Oprindelige folk er afgørende samarbejdspartnere for Verdens Skoves bevaringsarbejde i regnskovene. De bebor mange af verdens tilbageværende regnskove, og forskning viser, at skove bevares bedre, hvor oprindelige folk har jordrettigheder. Deres livsstil, mindre afhængig af industriel produktion og vækst, spiller en nøglerolle. Verdens Skove arbejder for at sikre juridiske rettigheder til skovene for oprindelige folk og presse for respekt fra nybyggere, virksomheder og myndigheder. </p>
+            <button className="text-orange pt-5">Læs mere →</button>
+          </div>
         </section>
       </main>
       <Footer className="p-0" />
