47f5ba098769af0d06b9678ac98aa788604ea061
Luuna Nørgaard <luun0004@stud.kea.dk>
2023-12-05 14:19:52 +0100

    ændringer

--- src/app/pages/forside/page.jsx
+++ src/app/pages/forside/page.jsx
@@ -15,153 +15,153 @@ import Footer from "../../components/Footer";
 const Card = () => {
   return (
     <>
-      <main className="leading-12">
+      <main className='leading-12'>
         <Navigation />
-        <section className="relative ">
-          <div className="relative lg:top-[40vh] md:top-[30vh] ">
-            <h1 className="absolute text-white p-5 pt-10 z-50 lg:max-w-[50vw] md:max-w-[45vw] lg:pl-[8vw] lg:pt-48 ">Red skovene, før planeten mister pusten </h1>
+        <section className='relative '>
+          <div className='relative lg:top-[40vh] md:top-[30vh] '>
+            <h1 className='absolute text-white p-5 pt-10 z-50 lg:max-w-[50vw] md:max-w-[45vw] lg:pl-[8vw] lg:pt-48 '>Red skovene, før planeten mister pusten </h1>
           </div>
-          <div className="relative">
-            <Image className="h-[62vh] lg:h-full lg:w-full" src={hero} alt="Picture of workers" />
+          <div className='relative'>
+            <Image className='h-[62vh] lg:h-full lg:w-full' src={hero} alt='Picture of workers' />
           </div>
-          <div className="lg:flex">
-            <div className="absolute top-52 lg:top-35 md:top-0 w-full h-full flex justify-center lg:justify-end md:justify-end md:pr-28 lg:pr-42s  items-center ">
-              <ProgressBar className="" />
+          <div className='lg:flex'>
+            <div className='absolute top-52 lg:top-35 md:top-0 w-full h-full flex justify-center lg:justify-end md:justify-end md:pr-28 lg:pr-42s  items-center '>
+              <ProgressBar className='' />
             </div>
           </div>
         </section>
 
-        <div className="bg-white p-4 pt-20 lg:p-32 md:p-4">
-          <section className="p-2 pt-40 lg:pt-0 pb-10 text-black">
-            <h2 className="">Hvem er Verdens Skove? </h2>
-            <p className="max-w-[1000px]">Verdens Skove er en dansk miljøorganisation, der gennem 40 år har arbejdet for en verden med en rig skovnatur gennem bl.a. bevarelse af skovene. Vores fokus er på klima, biodiversitet og de oprindelige folks rettigheder. </p>
-            <button className=" text-orange pt-2">Læs mere →</button>
+        <div className='bg-white p-4 pt-20 lg:p-32 md:p-4'>
+          <section className='p-2 pt-40 lg:pt-0 pb-10 text-black'>
+            <h2 className=''>Hvem er Verdens Skove? </h2>
+            <p className='max-w-[1000px]'>Verdens Skove er en dansk miljøorganisation, der gennem 40 år har arbejdet for en verden med en rig skovnatur gennem bl.a. bevarelse af skovene. Vores fokus er på klima, biodiversitet og de oprindelige folks rettigheder. </p>
+            <button className=' text-orange pt-2'>Læs mere →</button>
           </section>
-          <section className="w-full h-full border-t-2 border-black">
-            <div className=" flex-col justify-center pt-10 ">
+          <section className='w-full h-full border-t-2 border-black'>
+            <div className=' flex-col justify-center pt-10 '>
               <h2>Sådan kan du støtte vores arbejde</h2>
             </div>
 
-            <div className="grid gap-4 lg:flex md:flex sm:justify-center">
-              <div className="shadow-xl">
-                <Image src={truck} width={500} height={500} alt="Picture of workers" />
-                <div className="p-6">
+            <div className='grid gap-4 lg:flex md:flex sm:justify-center'>
+              <div className='shadow-xl'>
+                <Image src={truck} width={500} height={500} alt='Picture of workers' />
+                <div className='p-6'>
                   <h3>Doner til vores arbejde</h3>
                   <p>Giv et valgfrit beløb, og vær med til at redde skoven nu!</p>
-                  <button className="text-orange pt-16">Doner her →</button>
+                  <button className='text-orange pt-16'>Doner her →</button>
                 </div>
               </div>
-              <div className="shadow-xl">
-                <Image src={totebag} width={500} height={500} alt="Picture of totebag" />
-                <div className="p-6">
+              <div className='shadow-xl'>
+                <Image src={totebag} width={500} height={500} alt='Picture of totebag' />
+                <div className='p-6'>
                   <h3>Besøg vores webshop</h3>
                   <p>Støt os ved køb af plakater, kaffe og meget mere..</p>
-                  <button className="text-orange pt-16">Gå til webshop →</button>
+                  <button className='text-orange pt-16'>Gå til webshop →</button>
                 </div>
               </div>
-              <div className="shadow-xl ">
-                <Image src={certificat} width={500} height={500} alt="Picture of certificate" />
-                <div className="p-6">
+              <div className='shadow-xl '>
+                <Image src={certificat} width={500} height={500} alt='Picture of certificate' />
+                <div className='p-6'>
                   <h3>Regnskovscertifikat</h3>
                   <p>Køb et stykke jord og hjælp med at bevare regnskoven</p>
-                  <button className="text-orange pt-16">Køb her →</button>
+                  <button className='text-orange pt-16'>Køb her →</button>
                 </div>
               </div>
             </div>
-            <button className="text-orange pt-6">Se flere støttemuligheder →</button>
+            <button className='text-orange pt-6'>Se flere støttemuligheder →</button>
           </section>
-          <section className="mb-12"></section>
+          <section className='mb-12'></section>
           <section>
             <div>
-              <div className="bg-gray-200 p-5 lg:flex md:flex md:p-10 lg:p-10">
-                <Image className="h-auto w-full" src={bw} alt="Picture of the old days" />
-                <div className=" md:p-8">
-                  <h2 className="pt-12 md:pt-6 lg:pt-6">Hvordan arbejder vi? </h2>
+              <div className='bg-gray-200 p-5 lg:flex md:flex md:p-10 lg:p-10'>
+                <Image className='h-auto w-full' src={bw} alt='Picture of the old days' />
+                <div className=' md:p-8'>
+                  <h2 className='pt-12 md:pt-6 lg:pt-6'>Hvordan arbejder vi? </h2>
                   <p>Vi samarbejde med folk, som lever i og af skoven. Vi arbejder i områder med særlig høj biodiversitet. I dag har Verdens Skove indsatser i Danmark, Latinamerika og Østafrika. Verdens Skove samarbejder med lokale partnerorganisationer i det globale syd og har dedikeret gruppe frivillige i Danmark. </p>
-                  <button className="text-orange pt-2 md:pt-10 lg:pt-10">Læs mere →</button>
+                  <button className='text-orange pt-2 md:pt-10 lg:pt-10'>Læs mere →</button>
                 </div>
               </div>
             </div>
           </section>
-          <section className="lg:pb-28">
-            <h2 className="flex justify-center pt-12 lg:pt-28">Aktuelt nyt</h2>
-            <div className="flex overflow-scroll lg:justify-center">
-              <div className=" flex justify-center gap-4">
-                <div className=" w-80 shadow-xl">
-                  <Image src={pink} alt="Picture of origin people" />
-                  <div className="flex p-4 justify-between">
+          <section className='lg:pb-28'>
+            <h2 className='flex justify-center pt-12 lg:pt-28'>Aktuelt nyt</h2>
+            <div className='flex overflow-scroll lg:justify-center'>
+              <div className=' flex justify-center gap-4'>
+                <div className=' w-80 shadow-xl'>
+                  <Image src={pink} alt='Picture of origin people' />
+                  <div className='flex p-4 justify-between'>
                     <p>6 NOVEMBER 2023</p>
-                    <div className="flex">
-                      <svg className="" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                        <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                    <div className='flex'>
+                      <svg className='' xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt-fill' viewBox='0 0 16 16'>
+                        <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
                       </svg>
                       <p>Pico Bonito </p>
                     </div>
                   </div>
-                  <div className="p-4">
+                  <div className='p-4'>
                     <h3>Støtte af tolupanfolket</h3>
                     <p> Oprindelige folk og civilsamfundet i Honduras blev ikke kun udfordret af sundhedsrelateret pres under COVID-19 pandemien.</p>
-                    <button className="text-orange pt-2">Læs mere →</button>
+                    <button className='text-orange pt-2'>Læs mere →</button>
                   </div>
                 </div>
-                <div className=" w-80 shadow-xl">
-                  <Image src={people} alt="Picture of people in the forest" />
-                  <div className="flex p-4 justify-between">
+                <div className=' w-80 shadow-xl'>
+                  <Image src={people} alt='Picture of people in the forest' />
+                  <div className='flex p-4 justify-between'>
                     <p>20 NOVEMBER 2023</p>
-                    <div className="flex">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                        <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                    <div className='flex'>
+                      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt-fill' viewBox='0 0 16 16'>
+                        <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
                       </svg>
                       <p>Etiopien</p>
                     </div>
                   </div>
-                  <div className="p-4">
+                  <div className='p-4'>
                     <h3>Nyt initiativ: forbedring af skovovervågning</h3>
                     <p>Verdens Skove arbejder på et projekt i det sydvestlige Etiopien.</p>
 
-                    <button className="text-orange pt-2">Læs mere →</button>
+                    <button className='text-orange pt-2'>Læs mere →</button>
                   </div>
                 </div>
 
-                <div className="w-80 shadow-xl">
-                  <Image src={foresttruck} alt="Picture of forest and truck" />
-                  <div className="flex p-4 justify-between">
+                <div className='w-80 shadow-xl'>
+                  <Image src={foresttruck} alt='Picture of forest and truck' />
+                  <div className='flex p-4 justify-between'>
                     <p>18 SEPTEMBER 2023</p>
-                    <div className="flex">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                        <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                    <div className='flex'>
+                      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt-fill' viewBox='0 0 16 16'>
+                        <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
                       </svg>
                       <p>Bolivia</p>
                     </div>
                   </div>
-                  <div className="p-4">
+                  <div className='p-4'>
                     <h3>Tropiske skove forsvinder</h3>
                     <p>Nye tal viser en bekymrende udvikling i tab af skovdække i Bolivia. </p>
-                    <button className="text-orange pt-2">Læs mere →</button>
+                    <button className='text-orange pt-2'>Læs mere →</button>
                   </div>
                 </div>
-                <div className="w-80 shadow-xl">
-                  <Image src={foresttruck} alt="Picture of forest and truck" />
-                  <div className="flex p-4 justify-between">
-                    <p>18 SEPTEMBER 2023</p>
-                    <div className="flex">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                        <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                <div className='w-80 shadow-xl'>
+                  <Image src={foresttruck} alt='Picture of forest and truck' />
+                  <div className='flex p-4 justify-between'>
+                    <p>27 OKTOBER 2023</p>
+                    <div className='flex'>
+                      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-geo-alt-fill' viewBox='0 0 16 16'>
+                        <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
                       </svg>
-                      <p>Bolivia</p>
+                      <p>Costa Rica</p>
                     </div>
                   </div>
-                  <div className="p-4">
-                    <h3>Tropiske skove forsvinder</h3>
-                    <p>Nye tal viser en bekymrende udvikling i tab af skovdække i Bolivia. </p>
-                    <button className="text-orange pt-2">Læs mere →</button>
+                  <div className='p-4'>
+                    <h3>”Danskernes regnskov”</h3>
+                    <p>Nye tal viser en bekymrende udvikling i tab af skovdække i Bolivia. Landet er et af de steder i verden, kun overgået af Brasilien og Den Demokratiske Republik Congo. </p>
+                    <button className='text-orange pt-2'>Læs mere →</button>
                   </div>
                 </div>
               </div>
             </div>
           </section>
         </div>
-        <Footer className="p-0" />
+        <Footer className='p-0' />
       </main>
     </>
   );
--- src/app/pages/globaltarbejde/page.jsx
+++ src/app/pages/globaltarbejde/page.jsx
@@ -3,10 +3,13 @@ import Image from "next/image";
 import cover from "../../billeder/Globaltarbejde.png";
 import honduras from "../../billeder/honduras.png";
 import bolivia from "../../billeder/bolivia.png";
-import etiopien from "../../billeder/eteopien.png";
-import Nicaragua from "../../billeder/Nicaragua.png";
-import Uganda from "../../billeder/Uganda.png";
+import etiopien from "../../billeder/etiopien.png";
+import Nicaragua from "../../billeder/nicaragua.png";
+import Uganda from "../../billeder/uganda.png";
 import panama from "../../billeder/panama.png";
+import skov from "../../billeder/globalrarb1.png";
+import mandiflod from "../../billeder/flodmand.png";
+import Footer from "../../components/Footer";
 
 function globaltarbejde() {
   return (
@@ -23,17 +26,56 @@ function globaltarbejde() {
           </section>
           <section className=' bg-lys_grå'>
             <h2 className='p-3'>Her arbejder vi</h2>
-            <div className='flex overflow-scroll p-4 gap-4'>
-              <Image className='rounded-full border-2 border-gray-300 bg-white mb-4 w-32 h-32' src={honduras} alt='Picture of Honduras' />
-              <Image className='rounded-full p-2 border-2 border-gray-300 bg-white mb-4 w-32 h-32' src={bolivia} alt='Picture of Bolivia' />
-              <Image className='rounded-full p-2 border-2 border-gray-300 bg-white mb-4 w-32 h-32' src={etiopien} alt='Picture of Etiopien' />
-              <Image className='rounded-full p-2 border-2 border-gray-300 bg-white mb-4 w-32 h-32' src={Nicaragua} alt='Picture of Nicaragua' />
-              <Image className='rounded-full p-2 border-2 border-gray-300 bg-white mb-4 w-32 h-32' src={Uganda} alt='Picture of Uganda' />
-              <Image className='rounded-full p-2 border-2 border-gray-300 bg-white mb-4 w-32 h-32' src={panama} alt='Picture of Panama' />
+            <div className='flex overflow-scroll p-6 gap-5 '>
+              <div>
+                <Image className='rounded-full mb-4 border-gray border-2 ' src={honduras} alt='Picture of Honduras' />
+                <p>Honduras</p>
+              </div>
+              <div>
+                <Image className='rounded-full mb-4border-gray border-2' src={bolivia} alt='Picture of Bolivia' />
+                <p>Bolivia</p>
+              </div>
+              <div>
+                <Image className='rounded-full mb-4 border-gray border-2' src={etiopien} alt='Picture of Etiopien' />
+                <p>Etiopien</p>
+              </div>
+              <div>
+                <Image className='rounded-full mb-4 border-gray border-2' src={Nicaragua} alt='Picture of Nicaragua' />
+                <p>Nicaragua</p>
+              </div>
+              <div>
+                <Image className='rounded-full mb-4 border-gray border-2' src={Uganda} alt='Picture of Uganda' />
+                <p>Uganda</p>
+              </div>
+              <div>
+                <Image className='rounded-full mb-4 border-gray border-2' src={panama} alt='Picture of Panama' />
+                <p>Panama</p>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div>
+              <Image src={skov} alt='Picture of reainforest' />
+            </div>
+            <div className='lg:'>
+              <h2 className='pt-5'>Hvorfor er regnskovene vigtige?</h2>
+              <p>Regnskove er afgørende for planetens sundhed og menneskers overlevelse. De huser en unik biodiversitet, med over halvdelen af verdens arter, og er en kilde til medicin og fødevarer. Regnskovene regulerer lokalt og globalt klima ved at producere regn, binde CO2 og forhindre drivhusgasudledning. Desuden er de nøglen til ferskvand, og millioner af mennesker afhænger af dem for deres levebrød. Skovrydning truer ikke kun arter, men destabiliserer også økosystemer og påvirker klimaet. </p>
+              <button className='text-orange pt-5'>Læs mere →</button>
+            </div>
+          </section>
+          <section>
+            <div>
+              <Image src={mandiflod} alt='Picture of man in boat' />
+            </div>
+            <div>
+              <h2 className='pt-5'>Hvordan vi støtter de oprindelige folk</h2>
+              <p>Oprindelige folk er afgørende samarbejdspartnere for Verdens Skoves bevaringsarbejde i regnskovene. De bebor mange af verdens tilbageværende regnskove, og forskning viser, at skove bevares bedre, hvor oprindelige folk har jordrettigheder. Deres livsstil, mindre afhængig af industriel produktion og vækst, spiller en nøglerolle. Verdens Skove arbejder for at sikre juridiske rettigheder til skovene for oprindelige folk og presse for respekt fra nybyggere, virksomheder og myndigheder. </p>
+              <button className='text-orange pt-5'>Læs mere →</button>
             </div>
           </section>
         </section>
       </main>
+      <Footer className='p-0' />
     </>
   );
 }
