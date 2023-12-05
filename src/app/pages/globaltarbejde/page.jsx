c90509f5124602714eabb12d79e77cc9bfd2a772
Daniel Nygaard <dani12h1@stud.kea.dk>
2023-12-05 14:57:33 +0100

    Lande

--- src/app/pages/globaltarbejde/page.jsx
+++ src/app/pages/globaltarbejde/page.jsx
@@ -14,68 +14,68 @@ import Footer from "../../components/Footer";
 function globaltarbejde() {
   return (
     <>
-      <main className='leading-7 '>
-        <section className='relative'>
-          <Image src={cover} width={500} height={500} alt='Picture of global work' />
-          <h1 className='text-white absolute top-[7rem] left-2 '>Globlat Arbejde</h1>
+      <main className="leading-7 ">
+        <section className="relative">
+          <Image src={cover} className="w-full h-full" alt="Picture of global work" />
+          <h1 className="text-white absolute top-[7rem] left-2 ">Globlat Arbejde</h1>
         </section>
-        <section className='p-6'>
-          <section className=' pb-6'>
+        <section className="p-6">
+          <section className=" pb-6">
             <h2>Verdens Skove globalt</h2>
             <p>Gennem 30 år har Verdens Skove sammen med 200.000 danskere bevaret 88.300.000 m2, eller hvad der svarer til 12.362 fodboldbaner af regnskov i Costa Rica, Ecuador og Nicaragua. Dermed har Verdens Skove været med til at afværge den katastrofe, der truede regnskovene i slutningen af 80’erne.</p>
           </section>
-          <section className=' bg-lys_grå'>
-            <h2 className='p-3'>Her arbejder vi</h2>
-            <div className='flex overflow-scroll p-6 gap-5 '>
-              <div>
-                <Image className='rounded-full mb-4 border-gray border-2 ' src={honduras} alt='Picture of Honduras' />
-                <p>Honduras</p>
+          <section className="bg-lys_grå">
+            <h2 className="p-3">Her arbejder vi</h2>
+            <div className="flex overflow-scroll p-6 gap-5">
+              <div className="rounded-full">
+                <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px]  " src={honduras} alt="Picture of Honduras" />
+                <p className="flex justify-center">Honduras</p>
               </div>
               <div>
-                <Image className='rounded-full mb-4border-gray border-2' src={bolivia} alt='Picture of Bolivia' />
-                <p>Bolivia</p>
+                <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={bolivia} alt="Picture of Bolivia" />
+                <p className="flex justify-center"> Bolivia</p>
               </div>
               <div>
-                <Image className='rounded-full mb-4 border-gray border-2' src={etiopien} alt='Picture of Etiopien' />
-                <p>Etiopien</p>
+                <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={etiopien} alt="Picture of Etiopien" />
+                <p className="flex justify-center">Etiopien</p>
               </div>
               <div>
-                <Image className='rounded-full mb-4 border-gray border-2' src={Nicaragua} alt='Picture of Nicaragua' />
-                <p>Nicaragua</p>
+                <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={Nicaragua} alt="Picture of Nicaragua" />
+                <p className="flex justify-center">Nicaragua</p>
               </div>
               <div>
-                <Image className='rounded-full mb-4 border-gray border-2' src={Uganda} alt='Picture of Uganda' />
-                <p>Uganda</p>
+                <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={Uganda} alt="Picture of Uganda" />
+                <p className="flex justify-center">Uganda</p>
               </div>
               <div>
-                <Image className='rounded-full mb-4 border-gray border-2' src={panama} alt='Picture of Panama' />
-                <p>Panama</p>
+                <Image className="rounded-full mb-4 border-gray border-2 min-w-[120px] min-h-[120px] " src={panama} alt="Picture of Panama" />
+                <p className="flex justify-center">Panama</p>
               </div>
             </div>
           </section>
           <section>
             <div>
-              <Image src={skov} alt='Picture of reainforest' />
+              <Image src={skov} alt="Picture of reainforest" />
             </div>
-            <div className='lg:'>
-              <h2 className='pt-5'>Hvorfor er regnskovene vigtige?</h2>
+            <div className="lg:">
+              <h2 className="pt-5">Hvorfor er regnskovene vigtige?</h2>
               <p>Regnskove er afgørende for planetens sundhed og menneskers overlevelse. De huser en unik biodiversitet, med over halvdelen af verdens arter, og er en kilde til medicin og fødevarer. Regnskovene regulerer lokalt og globalt klima ved at producere regn, binde CO2 og forhindre drivhusgasudledning. Desuden er de nøglen til ferskvand, og millioner af mennesker afhænger af dem for deres levebrød. Skovrydning truer ikke kun arter, men destabiliserer også økosystemer og påvirker klimaet. </p>
-              <button className='text-orange pt-5'>Læs mere →</button>
+              <button className="text-orange pt-5">Læs mere →</button>
             </div>
           </section>
           <section>
             <div>
-              <Image src={mandiflod} alt='Picture of man in boat' />
+              <Image src={mandiflod} alt="Picture of man in boat" />
             </div>
             <div>
-              <h2 className='pt-5'>Hvordan vi støtter de oprindelige folk</h2>
+              <h2 className="pt-5">Hvordan vi støtter de oprindelige folk</h2>
               <p>Oprindelige folk er afgørende samarbejdspartnere for Verdens Skoves bevaringsarbejde i regnskovene. De bebor mange af verdens tilbageværende regnskove, og forskning viser, at skove bevares bedre, hvor oprindelige folk har jordrettigheder. Deres livsstil, mindre afhængig af industriel produktion og vækst, spiller en nøglerolle. Verdens Skove arbejder for at sikre juridiske rettigheder til skovene for oprindelige folk og presse for respekt fra nybyggere, virksomheder og myndigheder. </p>
-              <button className='text-orange pt-5'>Læs mere →</button>
+              <button className="text-orange pt-5">Læs mere →</button>
             </div>
           </section>
         </section>
       </main>
-      <Footer className='p-0' />
+      <Footer className="p-0" />
     </>
   );
 }
