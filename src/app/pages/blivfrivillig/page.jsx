1ffff44aca9b7e2be09bd0438fc79ce252d4d75a
Daniel Nygaard <dani12h1@stud.kea.dk>
2023-12-06 17:23:06 +0100

    flere sider

--- src/app/pages/blivfrivillig/page.jsx
+++ src/app/pages/blivfrivillig/page.jsx
@@ -11,26 +11,26 @@ function page() {
     <>
       <Navigation />
       <section className="relative">
-        <Image src={Frivillig} className="w-full h-full" alt="Picture of global work" />
+        <Image src={Frivillig} className="w-full h-full brightness-50" alt="Picture of global work" />
         <h1 className="text-white absolute top-[7rem] lg:pl-[6vw] pl-[6vw] lg:pt-[25vw]  md:pl-[6vw] md:pt-[20vw] pb-4">Bliv frivillig</h1>
       </section>
 
       <main className="leading-7 ">
-        <div className=" lg:p-0 mx-22 my-14">
-          <section className="p-3 lg:mx-44 lg:my-20">
-            <div className="mx-auto lg:max-w-[1000px]">
-              <h2 className="flex justify-center">Hvorfor blive frivillig hos Verdens Skove? </h2>
-              <p className="flex text-center pb-10 p-8 md:pb-20">Brænder du for en rig skovnatur? Synes du også skoven og naturen har alt for lidt plads? Både i Danmark, i vores bevidsthed og i Verden? Kunne du tænke dig at gøre noget ved det? Hvad end du er miljøaktivist, dokumentarist eller bare elsker skoven, så er du mere end velkommen som frivillig i Verdens Skove. Vi arbejder for den gode sag med at sætte skoven på dagsordenen i forskellige sammenhænge (se vores arbejdsgrupper nedenfor). Som frivillig i Verdens Skove kan du få erfaring med formidling, kampagneføring, eventmageri, fundraising, politisk arbejde og meget mere - alt imens du deltager i kampen for at redde verdens skove.</p>
+        <div className=" lg:p-0 mx-auto my-14 ">
+          <section className="p-3 lg:mx-44 lg:my-20 flex">
+            <div className="lg:max-w-[1000px] mx-auto">
+              <h2 className=" text-center  leading-8">Hvorfor blive frivillig hos Verdens Skove? </h2>
+              <p className="flex text-center pb-10 p-3 md:pb-20">Brænder du for en rig skovnatur? Synes du også skoven og naturen har alt for lidt plads? Både i Danmark, i vores bevidsthed og i Verden? Kunne du tænke dig at gøre noget ved det? Hvad end du er miljøaktivist, dokumentarist eller bare elsker skoven, så er du mere end velkommen som frivillig i Verdens Skove. Vi arbejder for den gode sag med at sætte skoven på dagsordenen i forskellige sammenhænge (se vores arbejdsgrupper nedenfor). Som frivillig i Verdens Skove kan du få erfaring med formidling, kampagneføring, eventmageri, fundraising, politisk arbejde og meget mere - alt imens du deltager i kampen for at redde verdens skove.</p>
             </div>
           </section>
         </div>
-        <div className="pb-32 md-">
+        <div className="pb-32 ">
           <Accordion className="" />
         </div>
 
-        <div className="lg:flex flex-row  gap-4 justify-center pb-20">
-          <Image src={Frivillig_2} className="" alt="Picture of global work" />
-          <Image src={Frivillig_2} className="" alt="Picture of global work" />
+        <div className="lg:flex flex-row  justify-center pb-20 p-3">
+          <Image src={Frivillig_2} className="mb-2 lg:mx-4" alt="Picture of global work" />
+          <Image src={Frivillig_2} className="mb-2 lg:mx-4" alt="Picture of global work" />
         </div>
       </main>
       <Footer />
