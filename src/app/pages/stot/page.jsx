838b82292ea9a405936e4b7286e6014476f936e6
Daniel Nygaard <dani12h1@stud.kea.dk>
2023-12-07 17:07:22 +0100

    støt

--- src/app/pages/stot/page.jsx
+++ src/app/pages/stot/page.jsx
@@ -14,64 +14,81 @@ const Stot = () => {
     setSelectedButton(button === setSelectedButton ? null : button);
   };
 
-  // Return the JSX structure
   return (
     <>
       <Navigation />
-
-      <div className="relative">
-        <Image src={hero} className="w-full h-[100vh]" alt="Picture of global work" />
-        <div className="absolute top-[14vh] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white pb-20 pt-16">
-          <h1 className="text-1xl font-bold">STØT VERDENS SKOVE</h1>
-          <p className="text-lg">Dit bidrag gør en forskel</p>
-        </div>
-
-        <div className="absolute left-1/2 top-1/2 p-8 bg-white shadow-md max-w-[600px] w-[80vw] h-[50vh] rounded-md bg-opacity-80 flex flex-col justify-center items-center space-y-4 -ml-[40vw] -mt-[25vh]">
-          <p className="text-black pb-4">Vælg hvordan du vil støtte</p>
-          <div className="">
-            <label className="flex flex-row gap-2">
-              <input className="accent-grå" type="radio" name="supportType" value="option1" />
-              Enkelt donation
-            </label>
-            <label className="flex flex-row gap-2">
-              <input className="accent-grå" type="radio" name="supportType" value="option2" />
-              Støt fast
-            </label>
-          </div>
-          <div className="flex gap-4 pt-5 justify-center">
-            <button className={`border border-1 rounded-md p-1 w-20 text-center text-grå ${selectedButton === 100 ? "bg-lys_grå text-grå" : "bg-white"}`} onClick={() => handleButtonClick(100)}>
-              100 kr
-            </button>
-            <button className={`border border-1 rounded-md p-1 w-20 text-center text-grå ${selectedButton === 200 ? "bg-lys_grå text-grå" : "bg-white"}`} onClick={() => handleButtonClick(200)}>
-              200 kr
-            </button>
-            <button className={`border border-1 rounded-md p-1 w-20 text-center text-grå ${selectedButton === 300 ? "bg-lys_grå text-grå" : "bg-white"}`} onClick={() => handleButtonClick(300)}>
-              300 kr
-            </button>
-          </div>
-          <div className="pt-4 ">
-            <label>
-              <input className="border border-1 rounded-md p-2 w-56 text-grå" placeholder="Valgfrit beløb" type="text" />
-            </label>
-          </div>
-          <form>
-            <div className="flex gap-2 pt-4 ">
-              <label className="flex flex-row gap-2">
-                <input className="accent-grå" type="radio" name="supportType" value="option1" />
-                Betalingskort
-              </label>
-              <label className="flex flex-row gap-2">
-                <input className="accent-grå" type="radio" name="supportType" value="option2" />
-                Mobilepay
-              </label>
+      {/* bg */}
+      <main className="bg-gray-400 h-[auto] w-[100vw] ">
+        {/* wrap om container 1 og 2 */}
+        <div className="grid lg:grid-rows-2 lg:grid-cols-2 justify-center grid-cols-1 grid-rows-1 lg: ">
+          {/* container 1 */}
+          <div className="lg:flex lg:flex-col grid grid-rows lg:items-center mt-22">
+            {/* boks 1 */}
+            <div className="bg-gray-400  mt-20">
+              <h1 className="text-center">STØT VERDENS SKOVE</h1>
+              <p className="text-center">Dit bidrag gør en forskel</p>
             </div>
-          </form>
-          <div className="pt-4">
-            <button className="bg-orange px-2 py-2 rounded-lg hover:bg-hover_orange text-white ">STØT NU</button>
+            {/* boks 2 */}
+            <div className=" p-10 bg-white rounded-lg bg-opacity-80 mx-8 my-10 lg:w-[600px] lg:h-[500px]">
+              <p className="text-black pb-4">Vælg hvordan du vil støtte</p>
+              <div className="">
+                <label className="flex flex-row gap-2">
+                  <input className="accent-grå" type="radio" name="supportType" value="option1" />
+                  Enkelt donation
+                </label>
+                <label className="flex flex-row gap-2">
+                  <input className="accent-grå" type="radio" name="supportType" value="option2" />
+                  Støt fast
+                </label>
+              </div>
+              <div className="flex gap-4 pt-5 ">
+                <button className={`border border-1 rounded-lg p-1 w-20 text-center text-grå ${selectedButton === 100 ? "bg-lys_grå text-grå" : "bg-white"}`} onClick={() => handleButtonClick(100)}>
+                  100 kr
+                </button>
+                <button className={`border border-1 rounded-lg p-1 w-20 text-center text-grå ${selectedButton === 200 ? "bg-lys_grå text-grå" : "bg-white"}`} onClick={() => handleButtonClick(200)}>
+                  200 kr
+                </button>
+                <button className={`border border-1 rounded-lg p-1 w-20 text-center text-grå ${selectedButton === 300 ? "bg-lys_grå text-grå" : "bg-white"}`} onClick={() => handleButtonClick(300)}>
+                  300 kr
+                </button>
+              </div>
+              <div className="pt-4 ">
+                <label>
+                  <input className="border border-1 rounded-lg p-2 w-56 text-grå" placeholder="Valgfrit beløb" type="text" />
+                </label>
+              </div>
+              <form>
+                <div className="flex gap-2 pt-4 ">
+                  <label className="flex flex-row gap-2">
+                    <input className="accent-grå" type="radio" name="supportType" value="option1" />
+                    Betalingskort
+                  </label>
+                  <label className="flex flex-row gap-2">
+                    <input className="accent-grå" type="radio" name="supportType" value="option2" />
+                    Mobilepay
+                  </label>
+                </div>
+              </form>
+              <div className="pt-4">
+                <button className="bg-orange px-2 py-2 rounded-lg hover:bg-hover_orange text-white ">STØT NU</button>
+              </div>
+            </div>
+          </div>
+          {/* container 2 */}
+          <div className="lg:flex lg:flex-col grid grid-rows  mt-22 ">
+            {/* boks 1 */}
+            <div className=" p-8 bg-white shadow-md rounded-lg bg-opacity-80 flex flex-col justify-center mx-8 my-2 lg:mt-28 h-[30vh] lg:w-[350px]">
+              <h3>Enkelt donation</h3>
+              <p>Alle donationer tæller, selv de helt små beløb. </p>
+            </div>
+            {/* boks 2 */}
+            <div className=" p-8 bg-white shadow-md rounded-lg bg-opacity-80 flex flex-col justify-center mx-8  my-10 h-[30vh] lg:w-[350px] ">
+              <h3>Støt fast</h3>
+              <p>Dit faste bidrag betyder, at vi kan lægge langsigtede strategier for vores indsats. På den måde er du med til at skabe et stærkt grundlag for vores arbejde.</p>
+            </div>
           </div>
         </div>
-        <div className="absolute left-1/2 top-1/2 p-8 bg-white shadow-md max-w-[600px] w-[80vw] h-[50vh] rounded-md bg-opacity-80 flex flex-col justify-center items-center space-y-4 -ml-[40vw] -mt-[50vh]"></div>
-      </div>
+      </main>
     </>
   );
 };
