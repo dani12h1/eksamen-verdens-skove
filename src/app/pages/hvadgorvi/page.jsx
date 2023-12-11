28eb5af87b0c30b29d1b00405498a153d6ef9151
Luuna Nørgaard <luun0004@stud.kea.dk>
2023-12-11 11:42:24 +0100

    caps

--- src/app/pages/hvadgorvi/page.jsx
+++ src/app/pages/hvadgorvi/page.jsx
@@ -18,80 +18,80 @@ function page() {
   return (
     <>
       <Navigation />
-      <main className="leading-7 ">
-        <section className="relative">
-          <Image src={cover1} className="w-full h-full brightness-[0.6]" alt="Picture of global work" />
-          <h1 className="text-white absolute top-[6rem] lg:pl-[6vw] pl-[6vw] lg:pt-[25vw]  md:pl-[6vw] md:pt-[20vw]">Hvad gør vi</h1>
+      <main className='leading-7 '>
+        <section className='relative'>
+          <Image src={cover1} className='w-full h-full brightness-[0.6]' alt='Picture of global work' />
+          <h1 className='text-white absolute top-[6rem] lg:pl-[6vw] pl-[6vw] lg:pt-[25vw]  md:pl-[6vw] md:pt-[20vw]'>Hvad gør vi</h1>
         </section>
 
-        <div className=" pb-4  md:m-22">
-          <section className="p-4 lg:p-14 md:p-12" style={{ textAlign: "center" }}>
-            <h2 className="pt-7 p-7">Hvordan arbejder vi? </h2>
-            <p className="mx-auto lg:pb-10 lg:max-w-[800px] md:max-w-[800px] p-5">Vi samarbejder med folk, som lever i og af skoven. I dag har Verdens Skove indsatser i Danmark, Latinamerika og Østafrika. Verdens Skove samarbejder med lokale partnerorganisationer i det globale syd og har dedikeret gruppe frivillige i Danmark. </p>
+        <div className=' pb-4  md:m-22'>
+          <section className='p-4 lg:p-14 md:p-12' style={{ textAlign: "center" }}>
+            <h2 className='pt-7 p-7'>Hvordan arbejder vi? </h2>
+            <p className='mx-auto lg:pb-10 lg:max-w-[800px] md:max-w-[800px] p-5'>Vi samarbejder med folk, som lever i og af skoven. I dag har Verdens Skove indsatser i Danmark, Latinamerika og Østafrika. Verdens Skove samarbejder med lokale partnerorganisationer i det globale syd og har dedikeret gruppe frivillige i Danmark. </p>
           </section>
           <section>
-            <div className="bg-gray-200 p-7 text-center">
-              <div className="flex flex-col  items-center lg:flex-row lg:pl-12">
-                <Image src={blad} className="w-8 h-18 " alt="leafsvg" />
-                <p className="ml-5 pb-4 mt-5 ">Vi bremser den enorme CO2-forurening, der kommer fra skovrydning.</p>
+            <div className='bg-gray-200 p-7 text-center'>
+              <div className='flex flex-col  items-center lg:flex-row lg:pl-12'>
+                <Image src={blad} className='w-8 h-18 ' alt='leafsvg' />
+                <p className='ml-5 pb-4 mt-5 '>Vi bremser den enorme CO2-forurening, der kommer fra skovrydning.</p>
               </div>
 
-              <div className="flex flex-col items-center lg:flex-row lg:pl-12">
-                <Image src={blad} className="w-8 h-18 " alt="leafsvg" />
-                <p className="ml-5 pb-4  mt-5">Vi bevare regnskoven sammen med de mennesker, der bor i og omkring skoven.</p>
+              <div className='flex flex-col items-center lg:flex-row lg:pl-12'>
+                <Image src={blad} className='w-8 h-18 ' alt='leafsvg' />
+                <p className='ml-5 pb-4  mt-5'>Vi bevare regnskoven sammen med de mennesker, der bor i og omkring skoven.</p>
               </div>
 
-              <div className="flex flex-col items-center lg:flex-row lg:pl-12">
-                <Image src={blad} className="w-8 h-18" alt="leafsvg" />
-                <p className="ml-5 pb-4  mt-5">Vi redde de tusindvis af dyre- og plantearter, der lever i regnskoven.</p>
+              <div className='flex flex-col items-center lg:flex-row lg:pl-12'>
+                <Image src={blad} className='w-8 h-18' alt='leafsvg' />
+                <p className='ml-5 pb-4  mt-5'>Vi redde de tusindvis af dyre- og plantearter, der lever i regnskoven.</p>
               </div>
 
-              <div className="flex flex-col items-center lg:flex-row lg:pl-12">
-                <Image src={blad} className="w-8 h-18" alt="leafsvg" />
-                <p className="ml-5 pb-4  mt-5">Vi skaber større rigdom af dyr og planter i Danmark.</p>
+              <div className='flex flex-col items-center lg:flex-row lg:pl-12'>
+                <Image src={blad} className='w-8 h-18' alt='leafsvg' />
+                <p className='ml-5 pb-4  mt-5'>Vi skaber større rigdom af dyr og planter i Danmark.</p>
               </div>
             </div>
           </section>
-          <section className="w-full h-full  mx-auto  lg:pt-20 p-12 mb-12 pb-10">
-            <div className=" flex-col justify-center pt-10 ">
-              <h2 className="flex justify-center">Vores arbejdsområder</h2>
+          <section className='w-full h-full  mx-auto  lg:pt-20 p-12 mb-12 pb-10'>
+            <div className=' flex-col justify-center pt-10 '>
+              <h2 className='flex justify-center'>Vores arbejdsområder</h2>
             </div>
 
-            <div className="gap-9 md:gap-3 lg:gap-10 lg:flex md:grid md:grid-cols-2 md:grid-rows-2 ">
-              <div className="shadow-xl rounded-lg mb-10">
-                <Image className="rounded-t-lg" src={vild} width={500} height={500} alt="Picture of nature " />
-                <div className="p-6 ">
+            <div className='gap-9 md:gap-3 lg:gap-10 lg:flex md:grid md:grid-cols-2 md:grid-rows-2 '>
+              <div className='shadow-xl rounded-lg mb-10'>
+                <Image className='rounded-t-lg' src={vild} width={500} height={500} alt='Picture of nature ' />
+                <div className='p-6 '>
                   <h3>Vild Dansk Natur</h3>
-                  <Link href="#" className="text-orange text-lg  ">
+                  <Link href='#' className='text-orange text-lg  '>
                     Læs mere →
                   </Link>
                 </div>
               </div>
 
-              <div className="shadow-xl rounded-lg mb-10">
-                <Image className="rounded-t-lg" src={politik} width={500} height={500} alt="Picture of politics" />
-                <div className="p-6">
+              <div className='shadow-xl rounded-lg mb-10'>
+                <Image className='rounded-t-lg' src={politik} width={500} height={500} alt='Picture of politics' />
+                <div className='p-6'>
                   <h3>Politisk Udvikling</h3>
-                  <Link href="" className="text-orange text-lg  ">
+                  <Link href='' className='text-orange text-lg  '>
                     Læs mere →
                   </Link>{" "}
                 </div>
               </div>
-              <div className="shadow-xl rounded-lg mb-10">
-                <Image className="rounded-t-lg" src={klima} width={500} height={500} alt="Picture of poster" />
-                <div className="p-6">
+              <div className='shadow-xl rounded-lg mb-10'>
+                <Image className='rounded-t-lg' src={klima} width={500} height={500} alt='Picture of poster' />
+                <div className='p-6'>
                   <h3>Klima</h3>
-                  <Link href="#" className="text-orange text-lg  ">
+                  <Link href='#' className='text-orange text-lg  '>
                     Læs mere →
                   </Link>
                 </div>
               </div>
 
-              <div className="shadow-xl rounded-lg mb-10">
-                <Image className="rounded-t-lg" src={globalt} width={500} height={500} alt="Picture of global work" />
-                <div className="p-6">
+              <div className='shadow-xl rounded-lg mb-10'>
+                <Image className='rounded-t-lg' src={globalt} width={500} height={500} alt='Picture of global work' />
+                <div className='p-6'>
                   <h3>Globalt Arbejde</h3>
-                  <Link href="globaltarbejde" className="text-orange text-lg  ">
+                  <Link href='globaltarbejde' className='text-orange text-lg  '>
                     Læs mere →
                   </Link>
                 </div>
@@ -100,45 +100,45 @@ function page() {
           </section>
         </div>
 
-        <section className="bg-gray-200 lg:justify-center p-5 lg:flex lg:flex-row md:flex-col md:p-10 mx-0 pb-16 lg:p-10">
-          <div className="p-5 lg:flex lg:flex-row md:flex-col md:p-10 lg:p-10 mx-0 rounded-2xl items-center">
-            <Image className="h-full w-full  lg:max-w-[400px] md:max-w-[500px] mx-auto" src={planetb} alt="Picture of there is not planet b" />
-            <div className="md:p-8 flex justify-center flex-col order-2 ">
-              <h2 className="pt-10 md:pt-6 ">Hvad kan du gøre? </h2>
-              <p className="max-w-[700px] ">ressourceforbrug, der overstiger planetens kapacitet, og CO2-udledning, der overbelaster atmosfæren. En gennemsnitsdansker kræver tre jordkloder. For at reducere klimapåvirkningen anbefales alternativer som cykling eller offentlig transport.</p>
-              <Link href="globaltarbejde" className="text-orange text-lg pt-4">
+        <section className='bg-gray-200 lg:justify-center p-5 lg:flex lg:flex-row md:flex-col md:p-10 mx-0 pb-16 lg:p-10'>
+          <div className='p-5 lg:flex lg:flex-row md:flex-col md:p-10 lg:p-10 mx-0 rounded-2xl items-center'>
+            <Image className='h-full w-full  lg:max-w-[400px] md:max-w-[500px] mx-auto' src={planetb} alt='Picture of there is not planet b' />
+            <div className='md:p-8 flex justify-center flex-col order-2 '>
+              <h2 className='pt-10 md:pt-6 '>Hvad kan du gøre? </h2>
+              <p className='max-w-[700px] '>ressourceforbrug, der overstiger planetens kapacitet, og CO2-udledning, der overbelaster atmosfæren. En gennemsnitsdansker kræver tre jordkloder. For at reducere klimapåvirkningen anbefales alternativer som cykling eller offentlig transport.</p>
+              <Link href='globaltarbejde' className='text-orange text-lg pt-4'>
                 Læs mere →
               </Link>
             </div>
           </div>
         </section>
 
-        <div className=" pb-6 lg:m-28 md:m-22">
-          <section className="  mx-auto   ">
-            <div className=" flex-col justify-center pt-3 ">
-              <h2 className="flex justify-center pt-16 lg:pb-20">Kampagner</h2>
+        <div className=' pb-6 lg:m-28 md:m-22'>
+          <section className='  mx-auto   '>
+            <div className=' flex-col justify-center pt-3 '>
+              <h2 className='flex justify-center pt-16 lg:pb-20'>Kampagner</h2>
             </div>
 
-            <div className="grid gap-10 md:gap-3 lg:gap-10 lg:flex md:flex sm:justify-center ">
-              <div className="shadow-xl rounded-lg">
-                <Image className="rounded-t-lg" src={hand} width={500} height={500} alt="Picture of hand " />
-                <div className="p-6">
+            <div className='grid gap-10 md:gap-3 lg:gap-10 lg:flex md:flex sm:justify-center '>
+              <div className='shadow-xl rounded-lg'>
+                <Image className='rounded-t-lg' src={hand} width={500} height={500} alt='Picture of hand ' />
+                <div className='p-6'>
                   <h3>KANALSAGEN</h3>
-                  <button className="text-orange">Læs mere →</button>
+                  <button className='text-orange'>Læs mere →</button>
                 </div>
               </div>
-              <div className="shadow-xl rounded-lg">
-                <Image className="rounded-t-lg" src={karlo} width={500} height={500} alt="Picture of development" />
-                <div className="p-6">
+              <div className='shadow-xl rounded-lg'>
+                <Image className='rounded-t-lg' src={karlo} width={500} height={500} alt='Picture of development' />
+                <div className='p-6'>
                   <h3>STOP KAFFE, DER RYDDER REGNSKOV</h3>
-                  <button className="text-orange">Læs mere →</button>
+                  <button className='text-orange'>Læs mere →</button>
                 </div>
               </div>
-              <div className="shadow-xl rounded-lg ">
-                <Image className="rounded-t-lg" src={plakat} width={500} height={500} alt="Picture of climate" />
-                <div className="p-6">
-                  <h3>Regnskoven om søerne</h3>
-                  <button className="text-orange ">Læs mere →</button>
+              <div className='shadow-xl rounded-lg '>
+                <Image className='rounded-t-lg' src={plakat} width={500} height={500} alt='Picture of climate' />
+                <div className='p-6'>
+                  <h3>REGNSKOVEN OM SØERNE</h3>
+                  <button className='text-orange '>Læs mere →</button>
                 </div>
               </div>
             </div>
