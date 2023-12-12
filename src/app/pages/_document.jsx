549c69b0f8632c0da8e754f701c60b9d407e8f82
Daniel Nygaard <dani12h1@stud.kea.dk>
2023-12-12 12:07:58 +0100

    hey

--- src/app/pages/_document.jsx
+++ src/app/pages/_document.jsx
@@ -1,6 +1,7 @@
 // pages/_document.jsx
 
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import favIcon from "../public";
 
 class MyDocument extends Document {
   render() {
@@ -10,7 +11,7 @@ class MyDocument extends Document {
           {/* Meta tags, stylesheets, and favicon */}
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="favicon.ico" />
+          <link rel="icon" href={favIcon} />
         </Head>
         <body>
           <Main />
